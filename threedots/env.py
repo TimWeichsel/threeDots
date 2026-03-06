a98815ca07aa89da4370e45eabc6d7125ccf426d
TimWeichsel <weichselberger.tim@icloud.com>
2026-03-06 09:56:26 +0100

    step function and helper functions updated to score game and not winning game!

--- threedots/env.py
+++ threedots/env.py
@@ -11,6 +11,10 @@ class MyEnv(gym.Env[np.ndarray, int]): #ObsType is array of 36 integers, ActType
         self.obstacle_default = obstacle_num
         self.board = None
         self.current_player = None
+        self.current_score = {"-1": 0, "1": 0}
+        self.current_score_positions = {"-1":[], "1": []} 
+        # determine truncated as always False (no time limit or other limitation)
+        self.truncated = False
 
     def valid_actions(self) -> list:
         return np.where(self.board == 0)[0].tolist()
@@ -24,85 +28,122 @@ class MyEnv(gym.Env[np.ndarray, int]): #ObsType is array of 36 integers, ActType
         self.board[obstacle_indices]=2
        
         self.current_player=1 #starting with player 1 and not -1
+        self.current_score = {"-1": 0, "1": 0}
+        self.current_score_positions = {"-1":[], "1": []} 
 
         observation = self.board.copy()
         info = {"current_player": self.current_player, "valid_actions": self.valid_actions()} 
         return observation, info
     
-    def _action_to_coordinates(action: int) -> tuple:
+    def _action_to_coordinates(self, action: int) -> tuple:
         row = action // 6 #int division for row
         column = action % 6 #modulo for column
         return row, column
     
     #Sliding window of 3 to check three in a axis
-    def _check_three_in_a_axis(self, positions: list) -> tuple[bool,Optional[list[int]]]:
+    def _check_three_in_a_axis(self, positions: list) -> tuple[int,Optional[list[int]]]:
+        newPoints = 0
+        newPointPositions = []
         for i in range(len(positions)-2):
             if self.board[positions[i]] == self.board[positions[i+1]] == self.board[positions[i+2]] == self.current_player: #Check if the three positions are the same and the current players mark
-                return True, list(positions[i:i+3])
-        return False, None
+                newPoints += 1
+                newPointPositions.append(positions[i:i+3])
+        return newPoints, newPointPositions
 
-    def _check_win(self, action: int) -> tuple[bool,Optional[list[int]]]:
+    def _check_new_points(self, action: int) -> tuple[int,Optional[list[int]]]:
         row, column = self._action_to_coordinates(action)
+        point_positions_h, point_positions_v, point_positions_d1, point_positions_d2 = [], [], [], []
 
         #horizontal
         if column == 0:
-            win, win_positions = self._check_three_in_a_axis([action, action+1, action+2])
+            newPoints_h, point_positions_h = self._check_three_in_a_axis([action, action+1, action+2])
         elif column == 1:
-            win, win_positions = self._check_three_in_a_axis([action-1, action, action+1, action+2])
+            newPoints_h, point_positions_h = self._check_three_in_a_axis([action-1, action, action+1, action+2])
         elif column == 4:
-            win, win_positions = self._check_three_in_a_axis([action-2, action-1, action, action+1])
+            newPoints_h, point_positions_h = self._check_three_in_a_axis([action-2, action-1, action, action+1])
         elif column == 5:
-            win, win_positions = self._check_three_in_a_axis([action-2, action-1, action])
+            newPoints_h, point_positions_h = self._check_three_in_a_axis([action-2, action-1, action])
         else:
-            win, win_positions = self._check_three_in_a_axis([action-2, action-1, action, action+1, action+2])
+            newPoints_h, point_positions_h = self._check_three_in_a_axis([action-2, action-1, action, action+1, action+2])
 
-        if win:
-            return True, win_positions
 
         #vertical
         if row == 0:
-            win, win_positions = self._check_three_in_a_axis([action, action+6, action+12])
+            newPoints_v, point_positions_v = self._check_three_in_a_axis([action, action+6, action+12])
         elif row == 1:
-            win, win_positions = self._check_three_in_a_axis([action-6, action, action+6, action+12])
+            newPoints_v, point_positions_v = self._check_three_in_a_axis([action-6, action, action+6, action+12])
         elif row == 4:
-            win, win_positions = self._check_three_in_a_axis([action-12, action-6, action, action+6])
+            newPoints_v, point_positions_v = self._check_three_in_a_axis([action-12, action-6, action, action+6])
         elif row == 5:
-            win, win_positions = self._check_three_in_a_axis([action-12, action-6, action])
+            newPoints_v, point_positions_v = self._check_three_in_a_axis([action-12, action-6, action])
         else:
-            win, win_positions = self._check_three_in_a_axis([action-12, action-6, action, action+6, action+12])
-
-        if win:
-            return True, win_positions
+            newPoints_v, point_positions_v = self._check_three_in_a_axis([action-12, action-6, action, action+6, action+12])
 
         steps_left1 = min(row, column, 2)
         steps_right1 = min(5-row, 5-column, 2)
-        win, win_positions = self._check_three_in_a_axis([action + i*7 for i in range(-steps_left1, steps_right1+1)])
+        newPoints_d1, point_positions_d1 = self._check_three_in_a_axis([action + i*7 for i in range(-steps_left1, steps_right1+1)])
 
-        if win:
-            return True, win_positions
 
         steps_left2 = min(row, 5-column, 2)
         steps_right2 = min(5-row, column, 2)
-        win, win_positions = self._check_three_in_a_axis([action + i*5 for i in range(-steps_left2, steps_right2+1)])
+        newPoints_d2, point_positions_d2 = self._check_three_in_a_axis([action + i*5 for i in range(-steps_left2, steps_right2+1)])
 
-        if win:
-            return True, win_positions
+        return newPoints_h+newPoints_v+newPoints_d1+newPoints_d2, point_positions_h+point_positions_v+point_positions_d1+point_positions_d2
+    
+    def _update_scores(self, newPoints: int, point_positions: list[int]):
+        self.current_score[str(self.current_player)] += newPoints
+        self.current_score_positions[str(self.current_player)].extend(point_positions)
 
-        return False, None
+    def _determine_winner(self) -> int:
+        if self.current_score["1"] > self.current_score["-1"]:
+            return 1
+        elif self.current_score["1"] < self.current_score["-1"]:
+            return -1
+        else:
+            return 0 #Tie
+        
+    def _current_info(self) -> dict:
+        return {"current_player": self.current_player, "valid_actions": self.valid_actions(), "current_score": self.current_score.copy(), "current_score_positions": {"-1": self.current_score_positions["-1"].copy(), "1": self.current_score_positions["1"].copy()}}
+
 
 
     def step(self, action: spaces.ActType):
-        # Perform players action and update board
-        self.board[action] = self.current_player
+        #check if action is valid
+        if action not in self.valid_actions():
+            observation = self.board.copy()
+            info = self._current_info()
+            reward = -1000
+            terminated = True
 
-        # Determine if the current player has won with this action
-        win, win_positions = self._check_win(action)
-        reward = 1 if win else None
+        else:
+            # Perform players action and update board
+            self.board[action] = self.current_player
 
-        # Check for Ddraw
-        if len(self.valid_actions()) == 0 and not win:
-            reward = 0
+            # Determine if the current player scored points from the current action
+            newPoints, point_positions = self._check_new_points(action)
+            self._update_scores(newPoints, point_positions)
+
+            reward = newPoints
+
+            # Check for End if game and determine winner/draw
+            terminated = len(self.valid_actions()) == 0
+            if terminated:
+                winner = self._determine_winner()
+                if winner == self.current_player:
+                    reward += 100
+                elif winner == self.current_player * -1:
+                    reward -= 100
+            
+                
+            # Switch Player
+            self.current_player *= -1
+            
+            # Obeservation is a copy of the current state of the board
+            observation = self.board.copy()
+
+            # Info contains the current player, the valid actions for the next player, the current scores and the positions of the points scored by each player
+            info = self._current_info()
+
+        return observation, reward, terminated, self.truncated, info
 
         
-
-        row, column = self._action_to_coordinates(action)
