4dbd8ea08f7a00b410d54606f9c20308c9044f8a
TimWeichsel <weichselberger.tim@icloud.com>
2026-03-11 11:55:44 +0100

    Included Buffer logic for gamestates

--- threedots/agents/DQNAgent.py
+++ threedots/agents/DQNAgent.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
 import numpy as np
+from collections import deque #buffer for action history
 
 class DQNAgent:
     def __init__(self, env: gym.Env, learning_rate: float, initial_epsilon: float,epsilon_decay: float,final_epsilon: float):
@@ -7,17 +8,25 @@ class DQNAgent:
         self.epsilon = initial_epsilon
         self.epsilon_decay = epsilon_decay
         self.final_epsilon = final_epsilon
+        self.buffer = deque(maxlen=10000)
+        self.last_observation = None #geet from update only the current observation
 
     def act(self, observation: np.ndarray, info: dict) -> int:
+        self.last_observation = observation #safe observation as buffer needs observation and last obersvation
         valid_actions = info["valid_actions"]
         if np.random.rand() < self.epsilon:
-            return np.random.choice(valid_actions)
+            action = np.random.choice(valid_actions)
+            return action
         else:
             pass
 
 
     
-    def update(self, observation: np.ndarray, action: int, reward: int, terminated: bool, info: dict):
+    def update(self, next_observation: np.ndarray, action: int, reward: int, terminated: bool, info: dict):
+
+
+
+        self.buffer.append((self.last_observation, action, reward, next_observation, terminated))
         pass
 
     def decay_epsilon(self):
